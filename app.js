ae9c562673e4dc5199112e7db6bb1edf0ba7dd42
xHanzza <cortegosogonzalo@gmail.com>
2022-07-04 20:51:42 -0300

    Primera Version Horrenda de Mercado Liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath))
+
+app.listen(3005, ()=> {console.log("El servidor esta VIVO")});
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
+
+app.get("/ofertas", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/ofertas.html"))
+})
+app.get("/register", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+})
+app.get("/login", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
